5e842edc135642f3de90dbc92df7dd85dd970b4e
cvnamrata <cnamrata.in@gmail.com>
2024-07-08 09:26:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// const searchForm = document.getElementById("form");//document.querySelector('form');
+let searchForm = document.getElementById("frm");
+const searchInput = document.querySelector('#search');
+const resultsList = document.querySelector('#results');
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchRecipes();
+})
+
+async function searchRecipes() {
+    const searchValue = searchInput.value.trim();
+    const response = await fetch(`https://api.edamam.com/search?q=${searchValue}&app_id=676e7141&app_key=4111317a79af0fd9e2d7d017ff34dd1a&from=0&to=10`);
+    const data = await response.json();
+    displayRecipes(data.hits);
+}
+
+function displayRecipes(recipes) {
+    let html = '';
+    recipes.forEach((recipe) => {
+        html += `
+        <div>
+            <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+            <h3>${recipe.recipe.label}</h3>
+            <ul>
+                ${recipe.recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
+            </ul>
+            <a href="${recipe.recipe.url}" target="_blank">View Recipe</a>
+        </div> 
+        `
+    })
+    resultsList.innerHTML = html;
+}
